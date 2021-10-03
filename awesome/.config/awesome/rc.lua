ae5c1fd8b60286859f786cb37eeedaca4481f5f4
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2021-10-03 20:25:26 +0530

    Minimal tasklist, no titles for awesomewm

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -233,11 +233,42 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
+	s.mytasklist = awful.widget.tasklist {
+		screen   = s,
+		filter   = awful.widget.tasklist.filter.currenttags,
+		buttons  = tasklist_buttons,
+		layout   = {
+			spacing_widget = {
+				valign = 'center',
+				halign = 'center',
+				widget = wibox.container.place,
+			},
+			spacing = 1,
+			layout  = wibox.layout.fixed.horizontal
+		},
+		-- Notice that there is *NO* wibox.wibox prefix, it is a template,
+		-- not a widget instance.
+		widget_template = {
+			{
+				wibox.widget.base.make_widget(),
+				id            = 'background_role',
+				widget        = wibox.container.background,
+			},
+			{
+				{
+					id     = 'clienticon',
+					widget = awful.widget.clienticon,
+				},
+				margins = 2,
+				widget  = wibox.container.margin
+			},
+			nil,
+			create_callback = function(self, c, index, objects) --luacheck: no unused args
+				self:get_children_by_id('clienticon')[1].client = c
+			end,
+			layout = wibox.layout.align.vertical,
+		},
+	}
 
     -- Create the wibox
 	custom_shape = function(cr, width, height)
