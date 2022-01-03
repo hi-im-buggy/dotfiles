8e3c0662447bc4478dd24fdf1fb742556a667819
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-01-03 22:29:53 +0530

    Add battery widget

--- awesome/.config/awesome/widgets/battery.lua
+++ awesome/.config/awesome/widgets/battery.lua
@@ -1,9 +1,46 @@
 local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
 
-local battery_widget = awful.widget.watch("acpi", 20, function(widget, stdout)
-	for percentage in stdout:gmatch("%d*%%") do
-		widget:set_text(" | Battery: " .. percentage .. " | ")
+local capacity_file = '/sys/class/power_supply/BAT0/capacity'
+local plugged_file = '/sys/class/power_supply/AC0/online'
+
+local battery_widget = wibox.widget {
+	-- {
+	-- 	id            = "myprogressbar",
+	-- 	min_value     = 0,
+	-- 	max_value     = 100,
+	-- 	value         = 0,
+	-- 	forced_width  = 50,
+	-- 	widget        = wibox.widget.progressbar,
+	-- },
+	{
+		id            = "mytextbox",
+		text          = "[unset]",
+		widget        = wibox.widget.textbox,
+	},
+	layout      = wibox.layout.stack,
+	set_battery = function(self, val, plug)
+		local symbol = ''
+		if tonumber(plug) == 1 then
+			symbol = '⚡️'
+		else
+			symbol = '🗲 '
+		end
+		self.mytextbox.markup = symbol .. tonumber(val) .. "%"
+		-- self.myprogressbar.value  = tonumber(val)
+	end,
+}
+
+gears.timer {
+	timeout   = 10,
+	call_now  = true,
+	autostart = true,
+	callback  = function()
+		local capacity = io.lines(capacity_file)()
+		local plugged = io.lines(plugged_file)()
+		battery_widget:set_battery(capacity, plugged)
 	end
-end)
+}
 
 return battery_widget
