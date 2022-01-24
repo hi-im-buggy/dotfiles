b3d22864a55c434b7198ffe6c86e09fdd9b3af15
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-01-24 21:33:51 +0530

    Add pause-play to now-playing widget

--- awesome/.config/awesome/widgets/now-playing.lua
+++ awesome/.config/awesome/widgets/now-playing.lua
@@ -1,5 +1,22 @@
 local awful = require("awful")
+local wibox = require("wibox")
+local gears = require("gears")
 
-local now_playing = awful.widget.watch("playerctl metadata --format 'Now Playing: {{title}} - {{artist}}'", 1)
+local now_playing = wibox.widget {
+	{
+		id = "myplayerwatch",
+		widget = awful.widget.watch(
+		"playerctl metadata --format ' {{title}} - {{artist}}'", 1)
+	},
+	layout = wibox.layout.stack,
+	
+	toggle_play_pause = (function()
+		awful.spawn.easy_async("playerctl play-pause")
+	end),
+}
+
+now_playing:buttons(gears.table.join(
+	awful.button({ }, 1, now_playing.toggle_play_pause)
+))
 
 return now_playing
