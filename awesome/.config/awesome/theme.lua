0bc7cd10c1e784d20317d8c010b497ae1e75d4c2
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2022-06-12 21:27:10 +0530

    Add permanent fallback wallpaper
    Since variety doesn't currently seem to be working properly

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -120,7 +120,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
--- theme.wallpaper = "/home/buggy/Downloads/6SQd2cV.jpg"
+theme.wallpaper = "/home/buggy/Pictures/variety-copied-wallpaper-a6c3173e26980e03d1e3aca15d893f6d.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
